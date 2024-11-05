f7cd0360771b70b8f80f36bfe6765531f182a0c1
Ethan H <ehallas@ithaca.edu>
2024-11-05 09:40:33 -0500

    Added Bottm Tab Nav

--- ICAccessibility/App.js
+++ ICAccessibility/App.js
@@ -24,6 +24,8 @@
 
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput, ImageBackground} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const HomeScreen = () => {
@@ -64,7 +66,7 @@ const HomeScreen = () => {
         </TouchableOpacity>
 
 
-        {/* Bottom Navigation */}
+        {/* Bottom Navigation 
         <View style={styles.bottomNav}>
   <TouchableOpacity style={styles.navButton}>
     <Icon name="home-outline" size={24} color="#FFFFFF" />
@@ -76,11 +78,58 @@ const HomeScreen = () => {
     <Icon name="settings-outline" size={24} color="#FFFFFF" />
   </TouchableOpacity>
 </View>
-
+*/}
       </View>
     </ImageBackground>
   );
 };
+
+const MapScreen = () => {
+  <Text>MAP</Text>
+}
+
+const SavedRoutesScreen = () => {
+  <Text>ROUTES</Text>
+}
+
+const Tab = createBottomTabNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home' : 'home-outline';
+            } else if (route.name === 'Map') {
+              iconName = focused ? 'location' : 'location-outline';
+            } else if (route.name === 'Routes') {
+              iconName = focused ? 'list' : 'list-outline';
+            }
+  
+           return <Icon name={iconName} size={size} color={color} />;
+          },
+	tabBarActiveTintColor: 'royalblue',
+   	tabBarInactiveTintColor: 'gray',
+    headerShown: false,
+    tabBarStyle: { position: 'absolute' },
+    tabBarBackground: () => (
+      <View style={[styles.navBar, {backgroundColor: 'black'}]}></View>
+    ),
+        })}
+>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Routes" component={SavedRoutesScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
 
@@ -179,6 +228,9 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     fontSize: 24,
   },
+  navBar: {
+    backgroundColor: '#000000',
+  }
 });
 
-export default HomeScreen;
+
